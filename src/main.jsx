c0ae9380ae169a70159d204463a33ddd33d158ef
Rachit Deshwal <157193302+RachitDeshwal@users.noreply.github.com>
2025-11-20 20:05:49 +0530

    Update server URL to production endpoint

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-export const serverURL = "http://localhost:8001";
+export const serverURL = "https://sneha-traders-backend.vercel.app/";
 import { BrowserRouter } from "react-router-dom";
 import { UserContextProvider } from "./contexts/UserContext.jsx";
 import ShopContext from "./contexts/ShopContext.jsx";
