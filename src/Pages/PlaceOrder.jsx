a8870e3b4818c831cab5ca06802d3a51275b8fbd
RachitDeshwal <rachitdeshwal0001@gmail.com>
2025-11-21 11:49:16 +0530

    fj

--- src/Pages/PlaceOrder.jsx
+++ src/Pages/PlaceOrder.jsx
@@ -262,7 +262,7 @@ function PlaceOrder() {
       </div>
 
       {/* RIGHT SIDE */}
-      <div className="lg:w-[45%] w-full flex flex-col items-center lg:items-start justify-start gap-[10px] max-w-[450px] sticky top-[120px] z-20">
+      <div className="lg:w-[45%] w-full flex flex-col items-center lg:items-start justify-start gap-[10px] max-w-[450px] mt-[-40px] md:mt-0">
         <CartTotal className="py-[20px]  " />
       </div>
     </div>
